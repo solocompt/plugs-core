7fa5c33d10dad2b0014f22d845352e69e30ec5b2
Ricardo Lobo <ricardolobo@audienciazero.org>
2016-12-20 18:43:13 +0000

    pins drf version to 3.3.3 to avoid error

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     author_email='ricardolobo@soloweb.pt',
     install_requires = [
         'django>=1.9.7',
-        'djangorestframework>=3.3.3',
+        'djangorestframework==3.3.3',
         'djangorestframework-jwt>=1.8.0',
         'requests>=2.2.1',
         'PyJWT>=1.4.0,<2.0.0',
