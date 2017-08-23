1048e1575a1da28216686019b99d3a4bc39a9537
beatrizseabra <beatrizseabra@soloweb.pt>
2017-08-23 18:42:15 +0100

    adds permission is owner or read only; changes is owner or read only to self or read only

--- plugs_core/permissions.py
+++ plugs_core/permissions.py
@@ -4,10 +4,10 @@ Core Permissions Classes
 
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class SelfOrReadOnly(permissions.BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
-    This only works for user objects, future implementations should extend this
+    ATTENTION: This only works for user objects, future implementations should extend this
     """
 
     def has_object_permission(self, request, view, obj):
@@ -17,3 +17,18 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj == request.user
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    This only works for objects related to a user through a field user
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
